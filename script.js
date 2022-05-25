9dd53eb14949a94cdabca773a5de8cb15d6909d9
blevinspercussion <blevinspercussion@gmail.com>
2022-05-25 12:01:28 -0500

    added function to parse json values

--- script.js
+++ script.js
@@ -2,10 +2,30 @@ const apiKey = "8b72d72bb036e3dd941b0c85d286e9d8";
 const urlBegin = "http://api.openweathermap.org/data/2.5/weather?q=";
 const urlMiddle = "&APPID=";
 
+let weatherData;
+let response;
+
 async function apiQuery(city) {
-  const response = await fetch(urlBegin + city + urlMiddle + apiKey);
-  const weatherData = await response.json();
-  console.log(weatherData);
+  response = await fetch(urlBegin + city + urlMiddle + apiKey, {
+    mode: "cors",
+  });
+
+  return await response.json();
 }
 
-apiQuery("Seattle");
+async function getMain(city) {
+  let data = await apiQuery(city);
+  let tempK = data.main.temp;
+  let tempC = Math.round((tempK - 273.15) * 100) / 100;
+  let tempF = Math.round((tempC * (9 / 5) + 32) * 100) / 100;
+  let mainWeather = data.weather[0].main;
+  let description = data.weather[0].description;
+  console.log(data);
+  console.log(tempK + "K");
+  console.log(tempC + "C");
+  console.log(tempF + "F");
+  console.log(mainWeather);
+  console.log(description);
+}
+
+getMain("Birmingham");
