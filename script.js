2e65ae81ff1fc42a58780aedc8a5072d4aad9389
blevinspercussion <blevinspercussion@gmail.com>
2022-05-26 10:49:14 -0500

    basic functionality of weatherCard is in place. displays temp (fahrenheit) and weather description

--- script.js
+++ script.js
@@ -11,7 +11,30 @@ const contentDiv = document.getElementById("content");
 let weatherData;
 let response;
 
-const drawDom = () => {};
+// Module to control weather card
+const weatherCardController = (() => {
+  const drawWeatherCard = (temp, description) => {
+    let weatherCard = document.createElement("div");
+    let weatherCardDate = document.createElement("h1");
+    let weatherCardTemp = document.createElement("p");
+    let weatherCardDescription = document.createElement("p");
+
+    weatherCard.classList.add("weather-card");
+    weatherCardDate.classList.add("date");
+    weatherCardTemp.classList.add("weather-body");
+    weatherCardDescription.classList.add("weather-body");
+
+    contentDiv.appendChild(weatherCard);
+    weatherCard.appendChild(weatherCardDate);
+    weatherCard.appendChild(weatherCardTemp);
+    weatherCard.appendChild(weatherCardDescription);
+
+    weatherCardDate.textContent = "Today";
+    weatherCardTemp.textContent = temp;
+    weatherCardDescription.textContent = description;
+  };
+  return { drawWeatherCard };
+})();
 
 async function apiQuery(city) {
   response = await fetch(urlBegin + city + urlMiddle + weatherApiKey, {
@@ -23,11 +46,12 @@ async function apiQuery(city) {
 
 async function getMain(city) {
   let data = await apiQuery(city);
-  let tempK = data.main.temp;
+  let tempK = await data.main.temp;
   let tempC = Math.round((tempK - 273.15) * 100) / 100;
   let tempF = Math.round((tempC * (9 / 5) + 32) * 100) / 100;
-  let mainWeather = data.weather[0].main;
-  let description = data.weather[0].description;
+  let mainWeather = await data.weather[0].main;
+  let description = await data.weather[0].description;
 
-  return data.main;
+  weatherCardController.drawWeatherCard(tempF, description);
 }
+getMain("Birmingham");
